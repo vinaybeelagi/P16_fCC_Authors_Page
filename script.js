c2dde7ca3a949f6c728337b2fa81a5b139074213
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-25 13:21:53 +0530

    Define variables to manage the index range for displaying authors and store author data

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get references to HTML elements
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+// Define variables to manage the index range for displaying authors and store author data
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
